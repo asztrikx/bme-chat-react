e26de0e4cd47bec636123881210f18204cdc034f
asztrikx <asztrik35@gmail.com>
2023-03-18 20:01:00 +0100

    2.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
+import { proxy } from "./Proxy";
 import './App.css';
 
 function App() {
+  console.log(proxy);
+
   return (
     <div className="App">
       <header className="App-header">
