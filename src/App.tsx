88c70a24ffe631fad44b1ad3e2cb96ee18821164
asztrikx <asztrik35@gmail.com>
2023-03-19 14:10:29 +0100

    3.8

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,21 @@ import logo from './logo.svg';
 import { proxy } from "./Proxy";
 import './App.css';
 import { Login } from './Login';
+import { Main } from './Main';
 
 export default class App extends Component {
+	state = { showLogin: true };
+
+	componentDidMount() {
+		proxy.addEventListener("login", () =>
+			this.setState({ showLogin: false })
+		);
+	}
+
 	render() {
 		return (
 			<div className="app">
-				<Login />
+				{ this.state.showLogin ? <Login /> : <Main /> }
 			</div>
 		);
 	}
