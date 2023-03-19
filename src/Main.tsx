f0c91294b4dd7959611e89201854c159b4125cae
asztrikx <asztrik35@gmail.com>
2023-03-19 13:11:19 +0100

    3.4

---
+++ src/Main.tsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+import { ConversationDto } from "./chat";
+
+export class Main extends Component {
+	state = { selectedConversation: undefined as ConversationDto | undefined };
+	render() {
+		return (
+			<div className="main row">
+				<LeftPane
+					inbox={proxy.inbox!}
+					selectedConversation={this.state.selectedConversation}
+					onSelect={(c) => this.setState({ selectedConversation: c })}
+				/>
+				<RightPane conversation={this.state.selectedConversation} />
+			</div>
+		);
+	}
+}
